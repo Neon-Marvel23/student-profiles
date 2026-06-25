d955a61b61091e5f99d9c83c9071dc5564289edf
Neon-Marvel23 <ndukamarvellous2@gmail.com>
2026-06-25 07:26:31 +0100

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,7 @@
-const CACHE_NAME = "csc413-v1";
-const urlsToCache = ["/", "/index.html", "/static/js/main.chunk.js"];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", () => {
+  console.log("PWA Installed");
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
 });
